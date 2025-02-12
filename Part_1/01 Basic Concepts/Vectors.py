966ba1867e1d64e2f83e2bcd56f5ed3659556cd9
Ibragim <ibrohim7477454@gmail.com>
2025-02-12 20:29:06 +0300

    Add program in Basic Concepts

--- Part_1/01 Basic Concepts/Vectors.py
+++ Part_1/01 Basic Concepts/Vectors.py
@@ -1,9 +1,25 @@
 def Vector_Components(cor_a, cor_b):
     delta_x = cor_b[0] - cor_a[0]
-    delta_y = cor_b[1] - cor_a[0]
+    delta_y = cor_b[1] - cor_a[1]  # Fixed mistake here
     return delta_x, delta_y
 
-v1 = (1,1)
+def Vector_Scaling(v1, a):
+    res_v = (v1[0] * a, v1[1] * a)
+    return res_v
+
+def Vector_Addition(v1, v2):
+    res_v = (v1[0] + v2[0], v1[1] + v2[1])
+    return res_v
+
+def Vector_Subtraction(v1, v2):
+    res_v = (v1[0] - v2[0], v1[1] - v2[1])  # Fixed mistake here
+    return res_v
+
+# Test cases
+v1 = (3,1)
 v2 = (5,4)
 
-print(Vector_Components(v1, v2))
+print("Vector Components:", Vector_Components(v1, v2))  # (2, 3)
+print("Scaled Vector:", Vector_Scaling(v2, 2))   # (10, 8)
+print("Vector Addition:", Vector_Addition(v1, v2))  # (8, 5)
+print("Vector Subtraction:", Vector_Subtraction(v1, v2))  # (-2, -3)
